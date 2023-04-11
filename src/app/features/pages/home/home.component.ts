a9814ddf76f054eb05a42bedbb11205fd9040b3b
mazerk01 <martinez.willy1@gmail.com>
2023-04-11 17:45:06 +0200

    bootstrap include

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <button type="button" class="btn">Base class</button>
+    <button type="button" class="btn btn-primary">Primary</button>
   `,
   styles: [
   ]

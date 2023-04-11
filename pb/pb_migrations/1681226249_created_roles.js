2ae513a22e9f3178545386af30aa91c31d898536
mazerk01 <martinez.willy1@gmail.com>
2023-04-11 17:19:27 +0200

    fix: backend restore

--- pb/pb_migrations/1681226249_created_roles.js
+++ pb/pb_migrations/1681226249_created_roles.js
@@ -1,15 +1,27 @@
 migrate((db) => {
   const collection = new Collection({
-    "id": "13a42v1k8lcocq1",
-    "created": "2023-04-07 13:55:19.760Z",
-    "updated": "2023-04-07 13:55:19.760Z",
+    "id": "0rtjqrf4rpi6z18",
+    "created": "2023-04-11 15:17:29.191Z",
+    "updated": "2023-04-11 15:17:29.191Z",
     "name": "roles",
     "type": "base",
     "system": false,
     "schema": [
       {
         "system": false,
-        "id": "8imjdws2",
+        "id": "hhfm43t1",
+        "name": "level",
+        "type": "number",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null
+        }
+      },
+      {
+        "system": false,
+        "id": "wtd8w2je",
         "name": "name",
         "type": "text",
         "required": false,
@@ -18,18 +30,6 @@ migrate((db) => {
           "min": null,
           "max": null,
           "pattern": ""
-        }
-      },
-      {
-        "system": false,
-        "id": "xavhiwae",
-        "name": "level",
-        "type": "number",
-        "required": false,
-        "unique": false,
-        "options": {
-          "min": null,
-          "max": null
         }
       }
     ],
@@ -45,7 +45,7 @@ migrate((db) => {
   return Dao(db).saveCollection(collection);
 }, (db) => {
   const dao = new Dao(db);
-  const collection = dao.findCollectionByNameOrId("13a42v1k8lcocq1");
+  const collection = dao.findCollectionByNameOrId("0rtjqrf4rpi6z18");
 
   return dao.deleteCollection(collection);
 })
